c088e59292910427a560cd02458bab3aaaea8a7c
mamacito93 <127861621+mamacito93@users.noreply.github.com>
2024-11-21 15:18:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/rapport/")
+@app.route("/histogramme/")
 def mongraphique():
     return render_template("graphique2.html")
   
