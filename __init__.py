12991c2acf25dc139010d94a51691659af2073c8
bakslamenace <127861453+bakslamenace@users.noreply.github.com>
2024-11-21 17:10:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,17 +4,13 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)  
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("forms.html")
-                                                                                                                                       
+app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
-  
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("forms.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -26,16 +22,21 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-  
-
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-
 @app.route("/histogramme/")
-def mongraphique2():
+def histogramme():
     return render_template("graphique2.html")
-
-
+ 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+ 
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
 if __name__ == "__main__":
   app.run(debug=True)
