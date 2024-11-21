e64f35d55a30dea11e8d0dbd73784184ec7f7027
bakslamenace <127861453+bakslamenace@users.noreply.github.com>
2024-11-21 15:42:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,20 +15,6 @@ def hello_world():
 def MaPremiereAPI():
     return render_template("forms.html")
 
-@app.route('/tawarano/')
-def meteo():
-    try:
-        response = urlopen('https://api.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')  # Remplacez 'xxx' par votre clé API
-        raw_content = response.read()
-        json_content = json.loads(raw_content.decode('utf-8'))
-        results = []
-        for list_element in json_content.get('list', []):
-            dt_value = list_element.get('dt')
-            temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
-            results.append({'Jour': dt_value, 'temp': temp_day_value})
-        return jsonify(results=results)
-    except Exception as e:
-        return jsonify({"error": str(e)})
 
 @app.route("/rapport/")
 def mongraphique():
