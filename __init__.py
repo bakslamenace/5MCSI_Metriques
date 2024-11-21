bc1f70b5bf27e2395ddf8a7a87cefc41bece3ab2
mamacito93 <127861621+mamacito93@users.noreply.github.com>
2024-11-21 16:05:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template, jsonify
-from urllib.request import urlopen
-import json
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
 from datetime import datetime
-from collections import Counter
-import requests
-
-app = Flask(__name__)
+from urllib.request import urlopen
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)  
 
 @app.route("/contact/")
 def MaPremiereAPI():
     return render_template("forms.html")
-
+                                                                                                                                       
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')  # comm2
-
+    return render_template('hello.html') #comm2
+  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -23,9 +23,10 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
 
 @app.route("/rapport/")
 def mongraphique():
@@ -37,4 +38,4 @@ def mongraphique2():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
