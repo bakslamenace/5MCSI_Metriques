5cba359cd3906607de3f40a2064a58b7368eda46
bakslamenace <127861453+bakslamenace@users.noreply.github.com>
2024-11-21 15:30:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
-from flask import Flask, rendertemplatestring, rendertemplate, jsonify
-from flask import rendertemplate
-from flask import json
-from datetime import datetime
+from flask import Flask, render_template, jsonify
 from urllib.request import urlopen
-import sqlite3
+import json
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
@@ -17,19 +14,22 @@ def MaPremiereAPI():
 
 @app.route('/tawarano/')
 def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
+    try:
+        response = urlopen('https://api.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')  # Remplacez 'xxx' par votre clé API
+        raw_content = response.read()
+        json_content = json.loads(raw_content.decode('utf-8'))
+        results = []
+        for list_element in json_content.get('list', []):
+            dt_value = list_element.get('dt')
+            temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
+            results.append({'Jour': dt_value, 'temp': temp_day_value})
+        return jsonify(results=results)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
 
-if __name == "__main":
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
